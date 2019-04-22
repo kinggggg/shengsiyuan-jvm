d01313aab5455edfcf4187d86c89861f2ce98307
zeek <zeek_beijing@126.com>
2019-04-22 23:27:46 +0800

    启动类加载器，扩展类加载器，应用类加载器加载类的路径

---
+++ src/main/java/shengsiyuan/MyTest18.java
@@ -0,0 +1,16 @@
+package shengsiyuan;
+
+/**
+ * @ClassName MyTest18
+ * @Description
+ * @Date 2019/4/22 11:20 PM
+ * @Version v1.0
+ **/
+public class MyTest18 {
+
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("sun.boot.class.path"));
+        System.out.println(System.getProperty("java.ext.dirs"));
+        System.out.println(System.getProperty("java.class.path"));
+    }
+}
