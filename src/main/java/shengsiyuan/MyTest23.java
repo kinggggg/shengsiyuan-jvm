1616b94b57dfa349cdc6f32e8ed9ba762ad09cab
zeek <zeek_beijing@126.com>
2019-05-09 23:33:29 +0800

    类命名空间考察

---
+++ src/main/java/shengsiyuan/MyTest23.java
@@ -0,0 +1,15 @@
+package shengsiyuan;
+
+/**
+ * 在运行期，一个Java类是由该类的完全限定名（binary name，二进制名）和用于加载该类的定义类加载器（defining loader）所共同决定的
+ * 如果同样名字（即相同的完全限定名）的类是由两个不同的加载器所加载，那么这些类就是不同的，即便.class文件的字节码完全一样，并且从相同的
+ * 位置加载亦是如此
+ **/
+public class MyTest23 {
+
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("sun.boot.class.path"));
+        System.out.println(System.getProperty("java.ext.dirs"));
+        System.out.println(System.getProperty("java.class.path"));
+    }
+}
