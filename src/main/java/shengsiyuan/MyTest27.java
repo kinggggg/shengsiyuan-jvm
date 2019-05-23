57bc1f257b73e7681a04e3ab5f27aa4cfdbed0b1
zeek <zeek_beijing@126.com>
2019-05-23 21:44:10 +0800

    30_线程上下文类加载器实战分析与难点剖析

---
+++ src/main/java/shengsiyuan/MyTest27.java
@@ -0,0 +1,20 @@
+package shengsiyuan;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * @ClassName MyTest27
+ * @Description
+ * @Date 2019/5/22 11:42 PM
+ * @Version v1.0
+ **/
+public class MyTest27 {
+
+    public static void main(String[] args) throws Exception {
+
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mytestdb", "username", "passowrd");
+
+    }
+}
