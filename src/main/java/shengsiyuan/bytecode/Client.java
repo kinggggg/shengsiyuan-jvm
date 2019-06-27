dcfeeb84f665828ee814986e8d6b23e33d612ce2
zeek <zeek_beijing@126.com>
2019-06-27 23:51:43 +0800

    实现java动态代理程序

---
+++ src/main/java/shengsiyuan/bytecode/Client.java
@@ -0,0 +1,19 @@
+package shengsiyuan.bytecode;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+public class Client {
+
+    public static void main(String[] args) {
+        System.getProperties().setProperty("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
+
+        RealSubject rs = new RealSubject();
+        InvocationHandler ds = new DynamicSubject(rs);
+        Class<?> cls = rs.getClass();
+
+        Subject subject = (Subject) Proxy.newProxyInstance(cls.getClassLoader(), cls.getInterfaces(), ds);
+
+        subject.request();
+    }
+}
---
+++ src/main/java/shengsiyuan/bytecode/Subject.java
@@ -0,0 +1,6 @@
+package shengsiyuan.bytecode;
+
+public interface Subject {
+
+    void request();
+}
