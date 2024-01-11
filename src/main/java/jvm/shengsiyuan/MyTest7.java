e8903b72a3b329ca18b3fb5faab2db75fef6b201
liweibo04 <liweibo04@58.com>
2024-01-11 23:24:45 +0800

    java.class.path环境变量取值

--- src/main/java/jvm/shengsiyuan/MyTest7.java
+++ src/main/java/jvm/shengsiyuan/MyTest7.java
@@ -11,6 +11,8 @@ public class MyTest7 {
 
         Class<?> clazz2 = Class.forName("jvm.shengsiyuan.C");
         System.out.println(clazz2.getClassLoader());
+
+        System.out.println(System.getProperty("java.class.path"));
     }
 
 }
