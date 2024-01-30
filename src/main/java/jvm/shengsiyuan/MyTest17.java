b3e209457772951958ea4ac8efddb90096ef4c44
liweibo04 <liweibo04@58.com>
2024-01-31 00:06:45 +0800

    类的加载

--- src/main/java/jvm/shengsiyuan/MyTest17.java
+++ src/main/java/jvm/shengsiyuan/MyTest17.java
@@ -9,13 +9,15 @@ package jvm.shengsiyuan;
 public class MyTest17 {
 
     public static void main(String[] args) throws Exception {
-        MyTest16_3 loader1 = new MyTest16_3("loader1");
+        MyTest16 loader1 = new MyTest16("loader1");
 
         Class<?> clazz = loader1.loadClass("jvm.shengsiyuan.MySample");
         System.out.println("class: " + clazz.hashCode());
 
-        // 如果注释掉改行，那么并不会实例化MySample对象，即MySample构造方法不会被调用
-        // 因此不会实例化MyCat对象，即没有对MyCat进行主动使用，这里就不会加载MyCat class（不一定，原因见笔记？？笔记呢？？？）
+        // 如果注释掉该行，那么并不会实例化MySample对象，即MySample构造方法不会被调用
+        // 因此不会实例化MyCat对象，即没有对MyCat进行主动使用，这里就不会加载MyCat class（虽然这里没有加载。但是这种情况
+        // 不是绝对的）。因为，『JVM规范允许类加载器在预料某个类将要被使用时就预先加载它』（来自有道笔记对应的PPT）
+        // 可以通过增加 -XX:TraceClassLoading 参数来验证上述行为。
         Object object = clazz.newInstance();
     }
 }
