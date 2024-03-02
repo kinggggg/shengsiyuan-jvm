ef031ff3dc6d273f49200aa9b044e09f94558f36
liweibo04 <liweibo04@58.com>
2024-03-02 11:30:22 +0800

    补充代码

--- src/main/java/jvm/shengsiyuan/MyTest24.java
+++ src/main/java/jvm/shengsiyuan/MyTest24.java
@@ -7,4 +7,11 @@ package jvm.shengsiyuan;
  * @Version v1.0
  **/
 public class MyTest24 {
+
+    public static void main(String[] args) {
+        // 输出 sun.misc.Launcher$AppClassLoader@18b4aac2
+        System.out.println(Thread.currentThread().getContextClassLoader());
+        // 输出 null
+        System.out.println(Thread.class.getClassLoader());
+    }
 }
